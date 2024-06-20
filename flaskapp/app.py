1f917f44fe8ec1fe0d1acac2ce0e862ba89890fb
manny <1eakanmu@gmail.com>
2024-06-19 20:23:51 -0400

    updated host

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -30,6 +30,5 @@ def process_form():
 @app.route('/success')
 def success():
     return redirect("https://www.youtube.com/feed/playlists")
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
