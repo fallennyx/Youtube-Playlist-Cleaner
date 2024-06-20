81d8125c5f1166e17bf970c84466823d9af0e6b8
manny <1eakanmu@gmail.com>
2024-06-19 21:29:06 -0400

    updated PORTs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from flask import Flask, render_template, request, redirect, url_for
@@ -38,4 +39,5 @@ def success():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
